e0ba19182bac3ff9366ebe6b0dde3915def8ed91
Eh1z <godsentehiz11@gmail.com>
2025-04-05 15:41:46 +0100

    cleaning house, changed my mind

--- src/components/sections/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Hero } from "./hero/hero";
-export { default as About } from "./about/about";
-export { default as Experience } from "./experience/experience";
-export { default as Contact } from "./contact/contact";
