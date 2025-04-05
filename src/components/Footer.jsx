a180380f348a2a105ed50da8c6c564d878cc32c2
Eh1z <godsentehiz11@gmail.com>
2025-04-05 20:21:57 +0100

    footer and faqs

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Footer = () => {
-	return <div>Footer</div>;
+	return (
+		<div className="w-full h-screen px-5 py-32 text-white bg-dark lg:px-8">
+			Footer
+		</div>
+	);
 };
 
 export default Footer;
--- src/sections/Faqs.jsx
+++ src/sections/Faqs.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 const Faqs = () => {
-	return <div>Faqs</div>;
+	return (
+		<div className="flex flex-col items-center w-full px-5 py-32 lg:px-8">
+			<div className="container ">
+				{/* headline */}
+				<span className="font-bold tracking-widest uppercase text-accent">
+					FAQS
+				</span>
+				<p className="text-4xl font-semibold leading-snug max-w-[900px]">
+					Do you have a question you want to ask?
+				</p>
+
+				{/*  */}
+
+				<div className="grid w-full grid-cols-5 gap-8 py-8">
+					{/* Image */}
+					<div className="col-span-2 bg-dark rounded-2xl h-[600px]" />
+					{/* Faqs */}
+					<div className="col-span-3 border rounded-2xl ">faqs</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Faqs;
